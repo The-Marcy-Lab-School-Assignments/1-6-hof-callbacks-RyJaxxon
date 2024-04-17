4f87388eeaf73878ad4aa359b0151955dcf50f3b
RyJaxxon <ryanjaxxon@gmail.com>
2024-04-16 21:33:31 -0400

    Created myForEach, myMap, myFind, myFilter, sortWords, and sortNumbers (1-6)

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,19 +1,46 @@
-const myForEach = () => {
+const myForEach = (myNames, callback) => {
+  for (let i = 0; i < myNames.length; i++) {
+    callback(myNames[i]);
+  };
 };
 
-const myMap = () => {
+const myMap = (arr, callback) => {
+  const newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    newArr.push(callback(arr[i]));
+  };
+  return newArr
 };
 
-const myFind = () => {
+
+const myFind = (arr, callback) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      return arr[i];
+      break
+    };
+  };
+  return undefined
 };
 
-const myFilter = () => {
+const myFilter = (arr, callback) => {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 };
 
-const sortWords = () => {
+const sortWords = (arr) => {
+  let newArr = [...arr]
+  return newArr.sort();
 };
 
-const sortNumbers = () => {
+const sortNumbers = (arr) => {
+  let newArr = [...arr]
+  return newArr.sort((a, b) => a - b);
 };
 
 const sortNumbersBetter = () => {
